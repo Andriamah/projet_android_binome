d04811474cdd76b224415ce5e713b422a6d1b0d4
tonyfenoaina <tfenoaina@gmail.com>
2023-08-07 20:42:57 +0300

    correction

--- Mobile/Projet/app/src/main/java/com/example/projetm1/view/fragment/favoris/FavorisFragment.java
+++ Mobile/Projet/app/src/main/java/com/example/projetm1/view/fragment/favoris/FavorisFragment.java
@@ -4,17 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.ListView;
 import android.widget.TextView;
 
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
-import com.example.projetm1.controller.FavoriController;
-import com.example.projetm1.databinding.FragmentGalleryBinding;
-import com.example.projetm1.model.Historique_favori;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.projetm1.R;
 import com.example.projetm1.adapter.FavoriAdapter;
@@ -25,26 +20,19 @@ import com.example.projetm1.model.Historique_favori;
 import java.util.ArrayList;
 
 
-import java.util.ArrayList;
-
-public class FavorisFragment extends Fragment implements FavoriController.GetFavoriClientCallBack {
+public class FavorisFragment extends Fragment {
 
     private FragmentGalleryBinding binding;
     private FavoriController favoriController;
 
-    private View root;
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        FavorisViewModel favorisViewModel =
+                new ViewModelProvider(this).get(FavorisViewModel.class);
+
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
-        root = binding.getRoot();
-
-        favoriController = new FavoriController();
-
-        // Remplacez 1 par l'ID du client que vous souhaitez récupérer les favoris
-        int idClient = 1;
-        favoriController.getListFavoriClient(idClient, this);
-
+        View root = binding.getRoot();
 
         View rootView = inflater.inflate(R.layout.fragment_gallery, container, false);
         favoriController = new FavoriController();
@@ -62,7 +50,6 @@ public class FavorisFragment extends Fragment implements FavoriController.GetFav
 
             }
         });
-
         return root;
     }
 
@@ -84,29 +71,4 @@ public class FavorisFragment extends Fragment implements FavoriController.GetFav
         super.onDestroyView();
         binding = null;
     }
-    private void creerListe(ArrayList<Historique_favori> lesFavori) {
-        if (lesFavori != null) {
-            requireActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                  /*  ListView listDesFavori = root.findViewById(R.id.listeViewFavori); ETO LAY LAYOUT MISY AN LE LISTvIEW
-                    FavoriAdapter adapter = new FavoriAdapter(requireContext(), lesFavori);
-                    listDesFavori.setAdapter(adapter);*/
-                }
-            });
-        }
-    }
-
-    @Override
-    public void onGetFavoriClientSuccess(ArrayList<Historique_favori> favoris) {
-        creerListe(favoris);
-    }
-
-    @Override
-    public void onGetFavoriClientFailure(String messageError) {
-        // Traiter l'échec de la récupération des favoris ici si nécessaire
-    }
-
-
-
 }
