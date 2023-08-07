be919e4128dd7253d84047dd17c74fe6aeb245d7
Andriamah <80816142+Andriamah@users.noreply.github.com>
2023-08-07 23:58:01 +0100

    llien

--- Mobile/Projet/app/src/main/java/com/example/projetm1/config/ApiConfig.java
+++ Mobile/Projet/app/src/main/java/com/example/projetm1/config/ApiConfig.java
@@ -2,7 +2,7 @@ package com.example.projetm1.config;
 
 public class ApiConfig {
     //public static final String BASE_URL = "http://192.168.56.1:5000";
-    public static final String BASE_URL = "http://192.168.56.1:5000";
+    public static final String BASE_URL = "https://kind-red-indri-sock.cyclic.app/";
 
 }
 
