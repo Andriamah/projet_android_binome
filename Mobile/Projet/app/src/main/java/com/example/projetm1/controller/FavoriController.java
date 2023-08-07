f0713c9ba522e80b3d2adf14a292f0ce925f0057
tonyfenoaina <tfenoaina@gmail.com>
2023-08-07 20:00:32 +0300

    test

--- Mobile/Projet/app/src/main/java/com/example/projetm1/controller/FavoriController.java
+++ Mobile/Projet/app/src/main/java/com/example/projetm1/controller/FavoriController.java
@@ -75,4 +75,46 @@ public class FavoriController {
         void onAddFavoriFailure(String message);
 
     }
+    public void getListFavoriClient(int id_client, final GetFavoriClientCallBack callBack) {
+        String favoriUrl = ApiConfig.BASE_URL + "/favori/" + id_client;
+
+        // Créer une instance d'OkHttpClient pour effectuer la requête
+        OkHttpClient client = new OkHttpClient();
+
+        // Construire la requête GET pour récupérer les détails du client
+        Request request = new Request.Builder()
+                .url(favoriUrl)
+                .get()
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                // Vérifier si la réponse est réussie (code de statut 200) avant de traiter la réponse
+                if (response.isSuccessful()) {
+                    // Récupérer la réponse JSON du serveur
+                    String jsonResponse = response.body().string();
+
+                    // Analyser la réponse JSON en une liste d'objets Favori à l'aide de Gson
+                    ArrayList<Historique_favori> favoris = new Gson().fromJson(jsonResponse, new TypeToken<ArrayList<Historique_favori>>() {}.getType());
+
+                    // Appeler le callback avec la liste de favoris
+                    callBack.onGetFavoriClientSuccess(favoris);
+                } else {
+                    // Appeler le callback avec un message d'erreur en cas de réponse non réussie
+                    callBack.onGetFavoriClientFailure("Erreur lors de la récupération des favoris du client");
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                // Appeler le callback avec un message d'erreur en cas d'échec de la requête
+                callBack.onGetFavoriClientFailure("Erreur lors de la récupération des favoris du client");
+            }
+        });
+    }
+    public interface  GetFavoriClientCallBack{
+        void onGetFavoriClientSuccess(ArrayList<Historique_favori> favoris);
+        void onGetFavoriClientFailure(String messageError);
+    }
 }
--- Mobile/Projet/app/src/main/java/com/example/projetm1/view/fragment/favoris/FavorisFragment.java
+++ Mobile/Projet/app/src/main/java/com/example/projetm1/view/fragment/favoris/FavorisFragment.java
@@ -10,12 +10,15 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.projetm1.controller.FavoriController;
 import com.example.projetm1.databinding.FragmentGalleryBinding;
 
 
 public class FavorisFragment extends Fragment {
 
     private FragmentGalleryBinding binding;
+    private FavoriController favoriController;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,8 +28,12 @@ public class FavorisFragment extends Fragment {
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        favorisViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        View rootView = inflater.inflate(R.layout.fragment_favori_list, container, false);
+        favoriController = new FavoriController();
+
+        // Appeler la méthode pour récupérer la liste de Favori
+        int idClient = 1; // Remplacez 1 par l'ID du client que vous souhaitez récupérer les favoris
+        favoriController.getListFavoriClient(idClient, this);
         return root;
     }
 
